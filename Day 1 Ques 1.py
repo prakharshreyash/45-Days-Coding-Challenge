d2269f8d70d2a7937ee36220745514f175d90863
Prakhar Shreyash <1905523@kiit.ac.in>
2022-05-03 22:49:03 +0530

    Update Day 1 Ques 1.py

--- Day 1 Ques 1.py
+++ Day 1 Ques 1.py
@@ -1,7 +1,14 @@
 // Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive. 
 // There is only one repeated number in nums, return this repeated number. 
 // You must solve the problem without modifying the array nums and uses only constant extra space.
-nums.sort()
-for i in nums:
-  if nums[i]==nums[i-1]:
-     return nums[i]
+
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        for i in nums:
+            if nums[i]==nums[i-1]:
+                return nums[i]
