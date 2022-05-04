c188bfd3cb161155ad1cd464c094ad2d63355702
Prakhar Shreyash <1905523@kiit.ac.in>
2022-05-04 22:23:32 +0530

    Create Day 2 Ques 1.py

---
+++ Day 2 Ques 1.py
@@ -0,0 +1,16 @@
+// Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
+
+// You must write an algorithm that runs in O(n) time and uses only constant extra space.
+
+class Solution(object):
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.sort()
+        res = []
+        for i in range(0, len(nums)-1):
+            if nums[i]==nums[i+1]:
+                res.append(nums[i])
+        return res
