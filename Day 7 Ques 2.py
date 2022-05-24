fe0248cff3d61573944a710326a9b52996612906
Prakhar Shreyash <1905523@kiit.ac.in>
2022-05-24 15:10:46 +0530

    Create Day 7 Ques 2.py

---
+++ Day 7 Ques 2.py
@@ -0,0 +1,14 @@
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        d = {}
+        for w in strs:
+            key = tuple(sorted(w))
+            d[key] = d.get(key, []) + [w]
+        return d.values()
